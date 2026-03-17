06e97652d9bd85213e5b1a2bcbcd04d8b8a8adb5
Siddhant Singh <141491564+siddie1@users.noreply.github.com>
2026-03-18 00:17:40 +0530

    export quiz

--- Diabetes-Assistance/backend/routes/exportRoutes.js
+++ Diabetes-Assistance/backend/routes/exportRoutes.js
@@ -4,10 +4,12 @@ import {
   exportAssessments,
 } from "../controllers/exportController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { exportQuizComparison } from "../controllers/exportController.js";
 
 const router = express.Router();
 
 router.get("/patients",protect, exportPatients);
 router.get("/assessments", protect, exportAssessments);
+router.get("/quiz-comparison", protect, exportQuizComparison);
 
 export default router;
